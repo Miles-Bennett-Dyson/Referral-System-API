8932e53823f4a05aad35a571acd5169a1c796fc6
Denis Gabdrakhmanov <rubik5000@ya.ru>
2026-01-17 17:17:09 +0500

    UserViewSet now inherits from RetrieveUpdateDestroyAPIView

--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from users.models import User
-from users.serializers import CreateUserSerializer, PhoneSerializer, VerifyCodeSerializer, InviteCodeSerializer
+from users.serializers import UserSerializer, PhoneSerializer, VerifyCodeSerializer, InviteCodeSerializer
 from users.services import send_sms_code, verify_and_auth_user, activate_invite_code
 
 
@@ -38,27 +39,6 @@ class VerifyCodeView(APIView):
 
         return Response(serializer.errors, status=400)
 
-class UserViewSet(viewsets.ModelViewSet):
-    serializer_class = CreateUserSerializer
-    queryset = User.objects.all()
-
-    # def get_permissions(self):
-    #     if self.action == "create":
-    #         self.permission_classes = (AllowAny,)
-    #
-    #     elif self.action in ["update", "partial_update", "destroy"]:
-    #         self.permission_classes = [IsAuthenticated]
-    #
-    #     return super().get_permissions()
-
-    # def get_serializer_class(self):
-    #     if self.action == "create":
-    #         return CreateUserSerializer
-    #     elif self.get_object() == self.request.user:
-    #         return UserSerializer
-    #     else:
-    #         return SecureUserData
-
 class ActivateInviteCodeView(APIView):
     def post(self, request):
         serializer = InviteCodeSerializer(data=request.data)
@@ -68,3 +48,10 @@ class ActivateInviteCodeView(APIView):
             activate_invite_code(user, invite_code)
             return Response({"message": "Код активирован"}, status=200)
         return Response(serializer.errors, status=400)
+
+class UserViewSet(RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def get_object(self):
+        return self.request.user
