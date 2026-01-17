3061c089cea72228e64c25a3f2736429fc8d4b5f
Denis Gabdrakhmanov <rubik5000@ya.ru>
2026-01-17 14:30:26 +0500

    Added a serializer for checking SMS codes.

--- users/serializers.py
+++ users/serializers.py
@@ -8,8 +8,24 @@ from users.validators import PhoneFieldValidator
 class PhoneSerializer(serializers.Serializer):
     phone_number = serializers.CharField(
         max_length=12,
+        min_length=12
     )
 
+    class Meta:
+        validators = [
+            PhoneFieldValidator()
+        ]
+
+class VerifyCodeSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(
+        max_length=12,
+        min_length=12
+    )
+    sms_code = serializers.IntegerField(
+        min_value=1000,
+        max_value=9999,
+        # error_messages="Введенный код имеет неверную длину."
+    )
     class Meta:
         validators = [
             PhoneFieldValidator()
@@ -21,11 +37,8 @@ class CreateUserSerializer(serializers.ModelSerializer):
         model = User
         fields = ("id", "phone_number", "invite_code", "referred_by")
 
-    def create(self, validated_data):
-        while True:
-            invite_code = generate_invite_code()
-            if not User.objects.filter(invite_code=invite_code).exists():
-                break
-        validated_data["invite_code"] = invite_code
-        user = User.objects.create(**validated_data)
-        return user
+class InviteCodeSerializer(serializers.Serializer):
+    invite_code = serializers.CharField(
+        max_length=6,
+        min_length=6,
+    )
